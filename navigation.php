cac1bd6211ea6ba28c248dae20b735df469e4ac9
marco76tv <marco.sottana@gmail.com>
2023-05-05 15:14:43 +0200

    .

--- navigation.php
+++ navigation.php
@@ -3,17 +3,6 @@
 declare(strict_types=1);
 
 return [
-    // 'Getting Started' => [
-    //     'url' => 'docs/getting-started',
-    //     'children' => [
-    //         'Customizing Your Site' => 'docs/customizing-your-site',
-    //         'Navigation' => 'docs/navigation',
-    //         'Algolia DocSearch' => 'docs/algolia-docsearch',
-    //         'Custom 404 Page' => 'docs/custom-404-page',
-    //     ],
-    // ],
-    // 'Jigsaw Docs' => 'https://jigsaw.tighten.co/docs/installation',
-
     'Pagine' => [
         'children' => [
             // 'Tipo di Registrazione' => 'docs/pages/type-of-sign-in',
