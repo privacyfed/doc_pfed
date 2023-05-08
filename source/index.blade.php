58cce109393e25f372d3cdf61e9ae2b0eb3d0992
marco76tv <marco.sottana@gmail.com>
2023-05-08 18:00:42 +0200

    .

--- source/index.blade.php
+++ source/index.blade.php
@@ -12,7 +12,7 @@
                     static docs quickly and easily.</p>
 
                 <div class="flex my-10">
-                    <a href="docs/getting-started" title="{{ $page->siteName }} getting started"
+                    <a href="{{ url('/docs/getting-started') }}" title="{{ $page->siteName }} getting started"
                         class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Get
                         Started</a>
 
@@ -22,14 +22,15 @@
                 </div>
             </div>
 
-            <img src="/assets/img/logo-large.svg" alt="{{ $page->siteName }} large logo" class="mx-auto mb-6 lg:mb-0 ">
+            <img src="{{ url('/assets/img/logo-large.svg') }}" alt="{{ $page->siteName }} large logo"
+                class="mx-auto mb-6 lg:mb-0 ">
         </div>
 
         <hr class="block my-8 border lg:hidden">
 
         <div class="md:flex -mx-2 -mx-4">
             <div class="mb-8 mx-3 px-2 md:w-1/3">
-                <img src="/assets/img/icon-window.svg" class="h-12 w-12" alt="window icon">
+                <img src="{{ url('/assets/img/icon-window.svg') }}" class="h-12 w-12" alt="window icon">
 
                 <h3 id="intro-laravel" class="text-2xl text-blue-900 mb-0">Templating with <br>Laravel's Blade engine</h3>
 
@@ -38,7 +39,7 @@
             </div>
 
             <div class="mb-8 mx-3 px-2 md:w-1/3">
-                <img src="/assets/img/icon-terminal.svg" class="h-12 w-12" alt="terminal icon">
+                <img src="{{ url('/assets/img/icon-terminal.svg') }}" class="h-12 w-12" alt="terminal icon">
 
                 <h3 id="intro-markdown" class="text-2xl text-blue-900 mb-0">Use Markdown for <br>content-driven pages</h3>
 
@@ -47,7 +48,7 @@
             </div>
 
             <div class="mx-3 px-2 md:w-1/3">
-                <img src="/assets/img/icon-stack.svg" class="h-12 w-12" alt="stack icon">
+                <img src="{{ url('/assets/img/icon-stack.svg') }}" class="h-12 w-12" alt="stack icon">
 
                 <h3 id="intro-mix" class="text-2xl text-blue-900 mb-0">Compile your assets <br>using Laravel Mix </h3>
 
